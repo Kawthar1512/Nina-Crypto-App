91e2bce75d27deadc7bf0c1aae18007928340f40
Kawthar Abolade <55846868+Kawthar1512@users.noreply.github.com>
2025-08-05 17:38:49 +0100

    styed the landing page

--- nina-wallet/src/components/Nav.jsx
+++ nina-wallet/src/components/Nav.jsx
@@ -2,11 +2,18 @@ import "../styles/LandingPage.css";
 import nina from "../assets/nina.png";
 export default function Nav() {
   return (
-    <nav className="homeNav bg-[#000] z-[1000] fixed border border-amber-400 ">
-      <div>
-        <img src={nina} alt="" className="w-[100px] h-[100px] object-contain" />
+    <nav className="homeNav bg-[#000] z-[1000] fixed  ">
+      <div className="flex item-center">
+        <div>
+          <img
+            src={nina}
+            alt=""
+            className="w-[100px] h-[100px] object-contain"
+          />
+        </div>
+        <h2>NINA</h2>
       </div>
-      <h2>Nina Wallet</h2>
+
       <div className="navLinks">
         <a href="">Features</a>
         <a href="">About</a>
--- nina-wallet/src/pages/Landing_Page.jsx
+++ nina-wallet/src/pages/Landing_Page.jsx
@@ -6,21 +6,19 @@ import Nav from "../components/Nav";
 import blob from "../assets/blob.png";
 import circle from "../assets/circle.png";
 import circle2 from "../assets/circle2.png";
+import { FaRocket } from "react-icons/fa"; // Rocket icon
 
 export default function LandPage() {
   return (
     <>
-      {/* <main className="bg-[#9266E3]  min-h-screen overflow-hidden w-full ">
-        <div className="container">
-         
-
-          <div className="imageDiv">
-            <img src={phone} alt="" />
-          </div>
-        </div>
-      </main> */}
       <main className=" hello h-screen w-full bg-black flex items-center justify-center">
-        <img src={circle2} alt="" className="absolute" />
+        <div className="relative"></div>
+        <img
+          src={circle2}
+          alt=""
+          className="absolute opacity-90 w-[100px] pointer-events-none "
+          style={{ animation: "spin 50s linear infinite" }}
+        />{" "}
         <div className=" bigi p-10  w-[900px] text-white  text-center">
           <div className=" text-center ">
             <h2 className="your_wallet font-bold">
@@ -28,13 +26,18 @@ export default function LandPage() {
               Seamless, Secure, Yours.
             </h2>
 
-            <div className="hanging-indent w-[300px] mx-auto">
+            <div className="hanging-indent w-[600px] mx-auto">
               Built for speed and simplicity. Nina Wallet lets you create
               Binance Smart Chain (BSC) wallets, send and receive BNB, and view
               your transaction history.
             </div>
 
-            <Link to="/login" className="started">
+            <Link
+              to="/login"
+              className=" mt-10 mx-auto inline-flex items-center bg-purple-600 text-white font-semibold py-3 
+  px-8 rounded-full shadow-md transition duration-300 ease-in-out transform hover:bg-purple-700 hover:scale-105 hover:shadow-xl"
+            >
+              <FaRocket className="mr-2" />
               Get Started
             </Link>
           </div>
