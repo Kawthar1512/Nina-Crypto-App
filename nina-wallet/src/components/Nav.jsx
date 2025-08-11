3ff1fed7671471588533bb331ea908247184be72
Kawthar Abolade <55846868+Kawthar1512@users.noreply.github.com>
2025-08-11 22:55:35 +0100

    font family

--- nina-wallet/src/components/Nav.jsx
+++ nina-wallet/src/components/Nav.jsx
@@ -4,18 +4,20 @@ import nina from "../assets/nina.png";
 export default function Nav() {
   return (
     <nav className="homeNav bg-[#0c0c0c] z-[1000] fixed  ">
-      <div className="flex flex-row items-center ml-[-200px] ">
+      <div className="flex flex-row items-center  border border-amber-100  text-center align-middle ">
         <img
           src={nina}
           alt="NINA logo"
-          className="w-[100px] h-[100px] object-contain  "
+          className="w-[100px] h-[100px] object-contain border border-amber-100  "
         />
-        <h2 className=" ml-1 font-bold text-2xl text-[#F3C738]">NINA WALLET</h2>
+        <h2 className=" nins  font-bold text-[22px] text-[#F3C738] w-[160px] border border-amber-100">NINA WALLET</h2>
       </div>
       <div className="navLinks text-gray-200">
         <a href="">Features </a>
         <a href="">About</a>
         <a href="">Download</a>
+        <a href="">FAQ</a>
+
       </div>
       <div className="but self-center">
         <button className="getStarted text-white bg-inherit px-[40px] py-[15px] cursor-pointer">
