d5228776ed490674cf85b95704c47c730d939322
Kawthar Abolade <55846868+Kawthar1512@users.noreply.github.com>
2025-08-11 18:11:49 +0100

    mockup

--- nina-wallet/src/components/Nav.jsx
+++ nina-wallet/src/components/Nav.jsx
@@ -13,17 +13,19 @@ export default function Nav() {
         <h2 className=" ml-1 font-bold text-2xl text-[#F3C738]">NINA</h2>
       </div>
       <div className="navLinks text-gray-200">
-        <a href="">Features</a>
+        <a href="">Features </a>
         <a href="">About</a>
         <a href="">Download</a>
       </div>
-      <button className="getStarted text-purple-900 bg-[#fbf2d4] px-[40px] py-[15px]">
-        Log in
-      </button>
+      <div className="but self-center">
+        <button className="getStarted text-white bg-inherit px-[40px] py-[15px] ">
+          Log in
+        </button>
 
-      <button className="getStarted bg-purple-900 text-[#F3C738] p-[15px]">
-        Download App
-      </button>
+        <button className="getStarted bg-purple-900 text-[#F3C738] p-[15px] ml-[15px]">
+          Sign up for free
+        </button>
+      </div>
     </nav>
   );
 }
