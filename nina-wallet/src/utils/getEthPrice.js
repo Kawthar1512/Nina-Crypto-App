c6549625ee977d2d5a5f68b5d1a436d807cbd1f4
Kawthar Abolade <55846868+Kawthar1512@users.noreply.github.com>
2025-09-08 00:28:40 +0100

    done

---
+++ nina-wallet/src/utils/getEthPrice.js
@@ -0,0 +1,12 @@
+export async function getEthPrice() {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+    );
+    const data = await response.json();
+    return data.ethereum.usd;
+  } catch (error) {
+    console.error("Failed to fetch ETH price:", error);
+    return null;
+  }
+}
